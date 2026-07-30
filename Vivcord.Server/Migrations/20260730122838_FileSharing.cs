8bfc93eac7d5c0860dca5bc73b81eac95e99c251
vlad <0309vlad2009@gmail.com>
2026-07-30 17:53:49 +0300

    File sharing migration

---
+++ Vivcord.Server/Migrations/20260730122838_FileSharing.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Vivcord.Server.Migrations
+{
+    /// <inheritdoc />
+    public partial class FileSharing : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "AttachmentType",
+                table: "UserMessages",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "AttachmentUrl",
+                table: "UserMessages",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "AttachmentType",
+                table: "UserMessages");
+
+            migrationBuilder.DropColumn(
+                name: "AttachmentUrl",
+                table: "UserMessages");
+        }
+    }
+}
